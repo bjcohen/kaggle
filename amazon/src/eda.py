3aca77d2f46af403d136b79cf6e135cf1ea07902
Ben Cohen <bj.cohen19@gmail.com>
2013-07-28 19:38:47 -0400

    amazon: svms, randomized lr

--- amazon/src/eda.py
+++ amazon/src/eda.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression, RidgeClassifier, RandomizedLogisticRegression
 from sklearn.feature_selection import RFE
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.metrics import auc_score
@@ -12,6 +12,7 @@ from sklearn.decomposition import RandomizedPCA
 from sklearn.cross_validation import cross_val_score
 from sklearn.cluster import Ward
 from sklearn.grid_search import GridSearchCV
+from sklearn.svm import SVC, NuSVC
 
 from operator import itemgetter
 from itertools import imap, combinations
@@ -66,22 +67,52 @@ if __name__ == '__main__':
     ohe.fit(np.vstack([train_data_nway, test_data_nway]))
 
     model_mat_train = ohe.transform(train_data_nway)
-
-    rfe = RFE(LogisticRegression(penalty='l2', dual=True, C=1., fit_intercept=True,
-                                 intercept_scaling=10., class_weight='auto'),
-                                 n_features_to_select=80000 ,step=.1)   #approx 2mm starting out
-
+    model_mat_test = ohe.transform(test_data_nway)
     ACTION = np.array(train_data.ACTION)
+
+    ### model training
     
+    rfe = RFE(LogisticRegression(penalty='l2', dual=True, C=1., fit_intercept=True, #approx 2mm starting out
+                                 intercept_scaling=10., class_weight='auto'))
+
+    ## logistic regression
+    
+    rfe.set_params(n_features_to_select=50000, step=.1)
     rfe.fit(model_mat_train, ACTION)
 
-    lr = LogisticRegression(penalty='l2', dual=True, C=10.,
-                            intercept_scaling=1., class_weight='auto')
+    lr = LogisticRegression(penalty='l2', dual=True, C=2.,
+                            intercept_scaling=50., class_weight='auto')
     lr.fit(model_mat_train[:27000, np.where(rfe.support_)[0]], ACTION[:27000])
     pred = lr.predict_proba(model_mat_train[27000:,np.where(rfe.support_)[0]])
     auc_score(ACTION[27000:], pred[:,1])
-    
-    rfc = RandomForestClassifier(max_depth=10, compute_importances=True, verbose=2)
-    rfc.fit(model_mat_train[:27000, np.where(rfe.support_)[0]], ACTION[:27000])
-    pred = rfc.predict_proba(model_mat_train[27000:,np.where(rfe.support_)[0]])
+
+    lr.fit(model_mat_train[:, np.where(rfe.support_)[0]], ACTION)
+    pred = lr.predict_proba(model_mat_test[:,np.where(rfe.support_)[0]])
+    pd.DataFrame({'Id' : test_data.index, 'Action' : pred[:,1]}).to_csv('../lr_submission.csv', header=True, index=False)
+
+    ## svms
+    svc = SVC(C=1., kernel='rbf', probability=True, class_weight='auto', verbose=2)
+    svc.fit(model_mat_train[:27000, np.where(rfe.support_)[0]], ACTION[:27000])
+    pred = svc.predict_proba(model_mat_train[27000:,np.where(rfe.support_)[0]])
     auc_score(ACTION[27000:], pred[:,1])
+
+    nusvc = NuSVC(nu=.5, kernel='rbf', probability=True, cache_size=1024, class_weight='auto', verbose=2)
+    nusvc.fit(model_mat_train[:27000, np.where(rfe.support_)[0]], ACTION[:27000])
+    pred = nusvc.predict_proba(model_mat_train[27000:,np.where(rfe.support_)[0]])
+    auc_score(ACTION[27000:], pred[:,1])
+
+    ## random forest
+    
+    rfe.set_params(n_features_to_select=10000, step=.1)
+    rfe.fit(model_mat_train, ACTION)
+    
+    rfc = RandomForestClassifier(max_depth=100, verbose=2)
+    rfc.fit(model_mat_train[:27000, np.where(rfe.support_)[0]].todense(), ACTION[:27000])
+    pred = rfc.predict_proba(model_mat_train[27000:,np.where(rfe.support_)[0]].todense())
+    auc_score(ACTION[27000:], pred[:,1])
+
+    gbc = GradientBoostingClassifier(loss='deviance', learning_rate=.1, n_estimators=100, max_depth=10, verbose=2)
+    gbc.fit(model_mat_train[:27000, np.where(rfe.support_)[0]].todense(), ACTION[:27000])
+    pred = gbc.predict_proba(model_mat_train[27000:,np.where(rfe.support_)[0]].todense())
+    auc_score(ACTION[27000:], pred[:,1])
+    
