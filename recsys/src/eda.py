fd45c691aaf5161bbd0bd26c87382c2a56f8e6fd
Ben Cohen <bj.cohen19@gmail.com>
2013-08-23 20:26:53 -0400

    recsys: notes and updates to eda for final data

--- recsys/src/eda.py
+++ recsys/src/eda.py
@@ -73,6 +73,26 @@ def get_test_data():
 
     return (business_data_test, review_data_test, user_data_test, checkin_data_test)
     
+def get_final_data():
+    business_final_fn = 'final_test_set_business.json'
+    review_final_fn = 'final_test_set_review.json'
+    user_final_fn = 'final_test_set_user.json'
+    checkin_final_fn = 'final_test_set_checkin.json'
+
+    final_directory = os.path.join('..', 'data')
+
+    business_data_final = read_streaming_json(os.path.join(final_directory, business_final_fn))
+    business_data_final.set_index('business_id', inplace=True)
+    review_data_final = read_streaming_json(os.path.join(final_directory, review_final_fn))
+    user_data_final = read_streaming_json(os.path.join(final_directory, user_final_fn))
+    user_data_final.set_index('user_id', inplace=True)
+    checkin_data_final = read_streaming_json(os.path.join(final_directory, checkin_final_fn))
+    checkin_data_final.set_index('business_id', inplace=True)
+
+    preprocess(business_data_final, review_data_final, user_data_final, checkin_data_final)
+
+    return (business_data_final, review_data_final, user_data_final, checkin_data_final)
+
 def preprocess(business_data, review_data, user_data, checkin_data):
     del business_data['neighborhoods']
 
@@ -97,6 +117,9 @@ def preprocess(business_data, review_data, user_data, checkin_data):
 if __name__ == '__main__':
     (bus_data, review_data, user_data, checkin_data) = get_train_data()
     (bus_data_test, review_data_test, user_data_test, checkin_data_test) = get_test_data()
+    (bus_data_final, review_data_final, user_data_final, checkin_data_final) = get_final_data()
+
+    review_data['age'] = review_data['date'].map(lambda x: (datetime.datetime(2013, 1, 19) - x).days)
 
     # business: full_address, lat, lng, name, state
     # user: name
@@ -110,7 +133,7 @@ if __name__ == '__main__':
     #   .join(checkin_data[['checkin_info']], on='business_id')
 
     # review: stars, date, votes, text
-    # businss: stars
+    # business: stars
     # user: average_stars, votes
     # model_mat_test = review_data_test \
     #   .join(bus_data_test[['review_count', 'open', 'categories', 'city']].rename(columns={'review_count' : 'review_count_bus'}), on='business_id') \
@@ -138,12 +161,88 @@ if __name__ == '__main__':
     #         pd.concat([checkin_data, checkin_data_test])))
 
     # reload(ngbr)
-    # ki = ngbr.KorenIntegrated(n_iter=10)
-    # ki.fit((pd.concat([bus_data, bus_data_test]), review_data.iloc[:180000], review_data.iloc[:220000], pd.concat([user_data, user_data_test]),
+    # ki = ngbr.KorenIntegrated(n_iter=1, model_type='integrated')
+    # ki.fit((pd.concat([bus_data, bus_data_test]), review_data.iloc[:100], review_data.iloc[:200], pd.concat([user_data, user_data_test]),
     #         pd.concat([checkin_data, checkin_data_test])))
-    # ki_pred = ki.predict(review_data.iloc[180000:220000])
+    # ki_pred = ki.predict(review_data.iloc[100:200])
+    # np.sqrt(mean_squared_error(review_data.ix[100:200,'stars'], ki_pred))
+
+    # reload(rbm)
+    # r = rbm.RBM(epochs=50, conditional=True, rating_levels=pd.Index([1,2,3,4,5]), learning_rate=0.01, lam=0.001, n_hidden=200, momentum=0.9)
+    # r.fit(bus_data.iloc[range(500) + [7637, 3038]], review_data.iloc[:2], review_data.iloc[:2])
+    # r_pred_proba = r.predict(review_data.iloc[:2], method='prob')
 
     reload(rbm)
-    r = rbm.RBM()
-    r.fit(pd.concat([bus_data, bus_data_test]), review_data.iloc[:100])
-    r.predict(review_data.iloc[:100], review_data.iloc[:100])
+    r = rbm.RBM(epochs=50, conditional=True, rating_levels=pd.Index([1,2,3,4,5]), learning_rate=0.01, lam=0.001, n_hidden=100, momentum=0.9)
+    r.fit(pd.concat([bus_data, bus_data_test, bus_data_final]),
+          review_data,
+          pd.concat([review_data, review_data_test, review_data_final]))
+    r_pred = r.predict(review_data_final, method='exp')
+    pd.DataFrame({'review_id' : review_data_final.index, 'stars' : np.maximum(0, np.minimum(5, r_pred))}).to_csv('../rbm_submission_e50_h100.csv',index=False)
+
+    reload(ngbr)
+    ki = ngbr.KorenIntegrated(n_iter=10, n_factors=100, model_type='integrated', gam3=0.002)
+    ki.fit((pd.concat([bus_data, bus_data_test, bus_data_final]),
+            review_data,
+            pd.concat([review_data, review_data_test, review_data_final]),
+            pd.concat([user_data, user_data_test]),
+            pd.concat([checkin_data, checkin_data_test, checkin_data_final])))
+    ki_pred = ki.predict(review_data_test)
+    pd.DataFrame({'review_id' : ki_pred.index, 'stars' : np.maximum(0, np.minimum(5, ki_pred))}).to_csv('../integrated_submission_i10_f100.csv', index=False)
+
+    reload(ngbr)
+    ki = ngbr.KorenIntegrated(n_iter=10, n_factors=100, model_type='svd++')
+    ki.fit((pd.concat([bus_data, bus_data_test, bus_data_final]),
+            review_data,
+            pd.concat([review_data, review_data_test, review_data_final]),
+            pd.concat([user_data, user_data_test]),
+            pd.concat([checkin_data, checkin_data_test, checkin_data_final])))
+    ki_pred = ki.predict(review_data_final)
+    pd.DataFrame({'review_id' : ki_pred.index, 'stars' : np.maximum(0, np.minimum(5, ki_pred))}).to_csv('../svdpp_submission_i10_f100.csv', index=False)
+
+    reload(ngbr)
+    ki = ngbr.KorenIntegrated(n_iter=10, n_factors=100, model_type='factor')
+    ki.fit((pd.concat([bus_data, bus_data_test, bus_data_final]),
+            review_data,
+            pd.concat([review_data, review_data_test, review_data_final]),
+            pd.concat([user_data, user_data_test]),
+            pd.concat([checkin_data, checkin_data_test, checkin_data_final])))
+    ki_pred = ki.predict(review_data_final)
+    pd.DataFrame({'review_id' : ki_pred.index, 'stars' : np.maximum(0, np.minimum(5, ki_pred))}).to_csv('../factor_submission_i10_f100.csv', index=False)
+
+    reload(ngbr)
+    ki = ngbr.KorenIntegrated(n_iter=10, n_factors=100, model_type='nsvd')
+    ki.fit((pd.concat([bus_data, bus_data_test, bus_data_final]),
+            review_data,
+            pd.concat([review_data, review_data_test]),
+            pd.concat([user_data, user_data_test]),
+            pd.concat([checkin_data, checkin_data_test, checkin_data_final])))
+    ki_pred = ki.predict(review_data_final)
+    pd.DataFrame({'review_id' : ki_pred.index, 'stars' : np.maximum(0, np.minimum(5, ki_pred))}).to_csv('../nsvd_submission_i10_f100.csv', index=False)
+
+    reload(ngbr)
+    ki = ngbr.KorenIntegrated(n_iter=10, n_factors=100, model_type='neighbors')
+    ki.fit((pd.concat([bus_data, bus_data_test, bus_data_final]),
+            review_data,
+            pd.concat([review_data, review_data_test, review_data_final]),
+            pd.concat([user_data, user_data_test]),
+            pd.concat([checkin_data, checkin_data_test, checkin_data_final])))
+    ki_pred = ki.predict(review_data_final)
+    pd.DataFrame({'review_id' : ki_pred.index, 'stars' : np.maximum(0, np.minimum(5, ki_pred))}).to_csv('../neighbors_submission_i10.csv', index=False)
+
+    reload(ngbr)
+    ki = ngbr.KorenIntegrated(n_iter=10, n_factors=100, model_type='timeneighbors')
+    ki.fit((pd.concat([bus_data, bus_data_test, bus_data_final]),
+            review_data,
+            pd.concat([review_data, review_data_test, review_data_final]),
+            pd.concat([user_data, user_data_test]),
+            pd.concat([checkin_data, checkin_data_test, checkin_data_final])))
+    ki_pred = ki.predict(review_data_final)
+    pd.DataFrame({'review_id' : ki_pred.index, 'stars' : np.maximum(0, np.minimum(5, ki_pred))}).to_csv('../timeneighbors_submission_i10.csv', index=False)
+    
+    # reload(nnmf)
+    # mf = nnmf.NNMF(item_sim='none', estimation_method='simu')
+    # mf.fit(user_data, bus_data, review_data.iloc[:100])
+    # mf_pred = mf.predict(review_data.iloc[:100])
+    
+    ## TODO: use IdLookupTable.csv to verify order
